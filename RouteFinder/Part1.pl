414988feeab2930e1fe2f03bbc7f79759bc16d6b
franguyarmani <fbpeabody@gmail.com>
2017-10-21 20:59:51 -0400

    update route finder

--- RouteFinder/Part1.pl
+++ RouteFinder/Part1.pl
@@ -8,8 +8,24 @@ edge('C','D',2000).
 edge('C','G',12).
 edge('G','F',800).
 edge('F','E',400).
-edge('E','D',3).
+edge('E','D',300).
+
 
 connected(X,Y,L) :- edge(X,Y,L) ; edge(Y,X,L).
 
-path(A,B,L,Path) :-
+path(StartNode,EndNode,Length,Path) :-
+    travel(StartNode,EndNode,Length,[A],Q)
+    reverse(.
+
+
+travel(Node1,Node2,Len,Path,[Node2|Path]) :-
+    connected(Node1,Node2,Len).
+travel(StartNode,EndNode,Len,Visited,Path) :-
+    connected(StartNode,AnotherNode,NodeDist),
+    AnotherNode \==EndNode,
+    \+member(AnotherNode,Visited),
+    travel(AnotherNode,EndNode,SubPathLength,[AnotherNode|Visited],Path),
+    Len is NodeDist+SubPathLength.
+
+
+
